10016afbb6d34acca0e439f71b66984a06fa2754
Gurkirat <gurkirat2773@gmail.com>
2024-09-25 14:12:00 -0600

    Adding API

--- littlelemon/littlelemon/urls.py
+++ littlelemon/littlelemon/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
 """
 from django.contrib import admin 
 from django.urls import path, include  
+from rest_framework.routers import DefaultRouter
+from restaurant import views
+
+#DefaultRouter.register(r'tables', views.BookingView)
  
 urlpatterns = [ 
     path('admin/', admin.site.urls), 
     path('restaurant/', include('restaurant.urls')),
+   # path('restaurant/booking/', include(DefaultRouter.urls)),
 ]
--- littlelemon/restaurant/views.py
+++ littlelemon/restaurant/views.py
@@ -1,5 +1,24 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework import generics
+from .models import Menu, Booking
+from .serializers import MenuSerializer, BookingSerializer
+
 
 # Create your views here.
 def index(request):
   return render(request, 'index.html', {})
+
+# Create your views here. 
+class MenuItemsView(generics.ListCreateAPIView):
+ queryset = Menu.objects.all()
+ serializer_class = MenuSerializer
+
+class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
+ queryset = Menu.objects.all()
+ serializer_class = MenuSerializer
+
+class BookingView(generics.ListCreateAPIView):
+  queryset = Booking.objects.all()
+  serializer_class = BookingSerializer
+ 
